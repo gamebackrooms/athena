c2e8180436c2651f196ff4fd22984c19d1b933ce
Best Damn Cat <bestdamncat@gmail.com>
2024-11-17 21:50:24 -0800

    update

--- store/views.py
+++ store/views.py
@@ -1256,7 +1256,7 @@ def verify_signature_game(request):
             verify_key.verify(message_bytes, signature_bytes)
             print("Signature is valid!")
  
-            url = "https://worldchain-mainnet.g.alchemy.com/v2/brUu7bUWYqnL02KEqM_k1GWoLgTtkGvg"
+            url = "https://solana-mainnet.g.alchemy.com/v2/t7AGL7qRXHF4jvodUVH7gWn3lvSfk_jl"
             headers = {"accept": "application/json", "content-type": "application/json"}
 
             payload = {
