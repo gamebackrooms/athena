fe587510d7e85c860b097721a0ea8041fc4bf815
Best Damn Cat <bestdamncat@gmail.com>
2024-11-28 12:54:58 -0800

    update

--- store/views.py
+++ store/views.py
@@ -1571,7 +1571,7 @@ def marketcap_json(request):
         if search_name and search_value:
             # Using **kwargs to dynamically filter by search_name and search_value
             filter_kwargs = {search_name: search_value}
-            tokens = PumpFunToken.objects.filter(**filter_kwargs).order_by('-created_timestamp')[:15]
+            tokens = PumpFunToken.objects.filter(**filter_kwargs).order_by('-created_timestamp')[:13]
         elif search_value:
             # Perform a like search on specific fields
             tokens = PumpFunToken.objects.filter(
@@ -1584,7 +1584,7 @@ def marketcap_json(request):
                 Q(website__icontains=search_value)
             ).order_by('-created_timestamp')[:15]            
         else:
-            tokens = PumpFunToken.objects.order_by('-created_timestamp')[:15]
+            tokens = PumpFunToken.objects.order_by('-created_timestamp')[:10]
 
 
         total_token_count = PumpFunToken.objects.count()
